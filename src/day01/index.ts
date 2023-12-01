dedadae88eec5d033ec183aa00230946661a72dd
Michael Uloth <hello@michaeluloth.com>
2023-12-01 17:35:37 -0500

    day one

--- src/day01/index.ts
+++ src/day01/index.ts
@@ -4,15 +4,20 @@ const parseInput = (rawInput: string) => rawInput
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput)
-  console.log('input', input)
 
-  return
+  // Convert the text to an array of strings (where each line becomes one item)
+  const array = input.split('\n')
+
+  return sumOfCalibrationValues(array)
 }
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput)
 
-  return
+  // Convert the text to an array of strings (where each line becomes one item)
+  const array = input.split('\n')
+
+  return sumOfCalibrationValues(array)
 }
 
 run({
@@ -27,13 +32,77 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      { input: 'two1nine', expected: 29 },
+      { input: 'eightwothree', expected: 83 },
+      { input: 'abcone2threexyz', expected: 13 },
+      { input: 'xtwone3four', expected: 24 },
+      { input: '4nineeightseven2', expected: 42 },
+      { input: 'zoneight234', expected: 14 },
+      { input: '7pqrstsixteen', expected: 76 },
     ],
     solution: part2,
   },
   trimTestInputs: true,
   onlyTests: false,
 })
+
+const sumOfCalibrationValues = (array: string[]): number =>
+  array.reduce((total, item) => {
+    // Find all digits in the string
+    const digits = getNumberAndTextDigits(item)
+    // console.log('digits', digits);
+
+    // Find the first digit (if any)
+    const firstDigit = digits.length ? digits[0] : 0
+    // console.log('firstDigit', firstDigit)
+
+    // Find the last digit (if any)
+    const lastDigit = firstDigit ? digits[digits.length - 1] : ''
+    // console.log('lastDigit', lastDigit)
+
+    // Combine two digits into a single two-digit calibration number
+    const twoDigitNumber = Number(firstDigit + lastDigit)
+    console.log('twoDigitNumber', twoDigitNumber)
+
+    // Update the total
+    return total + twoDigitNumber
+  }, 0)
+
+function getNumberAndTextDigits(str: string): string[] {
+  console.log('str', str)
+
+  // Find all digits and digits written as text (e.g. "one", "two", "three"...) in a string
+  const regex = /(\d|(one|two|three|four|five|six|seven|eight|nine))/g
+
+  // Find all matches
+  const matchesAsArray = str.match(regex) || []
+
+  // Convert text digits to number digits
+  const matchesAsStringDigits = matchesAsArray.map((match): string => {
+    switch (match) {
+      case 'one':
+        return '1'
+      case 'two':
+        return '2'
+      case 'three':
+        return '3'
+      case 'four':
+        return '4'
+      case 'five':
+        return '5'
+      case 'six':
+        return '6'
+      case 'seven':
+        return '7'
+      case 'eight':
+        return '8'
+      case 'nine':
+        return '9'
+      default:
+        return match
+    }
+  })
+
+  // Return an array of all matches
+  return matchesAsStringDigits
+}
